814c4a69247565bf0543ff1433c4fa7cbd4f1a4f
Phoenix Campanile <phoenixcampanile@gmail.com>
2018-11-19 15:30:32 -0500

    Added a Sword collision
    Made simple scene with a rigged dummy, named larry, who does a simple animation when hit by the sword.

---
+++ VRSword/Assets/LarryController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LarryController : MonoBehaviour
+{
+
+    public bool playHitAnim = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playHitAnim){
+            if(GetComponent<Animation>().isPlaying){
+                return;
+            } 
+            GetComponent<Animation>().Play();
+            playHitAnim = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision){
+        Debug.Log(collision.gameObject.name);
+        playHitAnim = true;
+    }
+}
