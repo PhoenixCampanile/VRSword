7874c4f975d35fe0990952a333f5df0d0043729c
Unknown <phoenixcampanile@gmail.com>
2019-01-12 21:32:50 -0500

    Improved Hand And Stuff
    Ah

---
+++ VRSword/Assets/CollisionDetector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+
+
+    public bool colliding = false;
+    public GameObject colobj; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+        this.colliding = true;
+        this.colobj = collision.gameObject;
+    }
+
+    void OnCollisionExit(Collision collision){
+        this.colliding = false;
+        this.colobj = null;
+    }
+
+    public GameObject GetCollidedObject(){
+        return this.colobj;
+    }
+}
--- VRSword/Assets/Movement.cs
+++ VRSword/Assets/Movement.cs
@@ -12,8 +12,10 @@ public class Movement : MonoBehaviour
 
     public Transform head;
     public Transform controller;
+    public GameObject hand;
+    public GameObject gripPoint;
 
-    public GameObject sword;
+    public GameObject equippedWeapon;
     public GameObject handle;
     public Text uiText;
     public float speed;
@@ -34,32 +36,91 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(Application.isEditor){
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            transform.Translate(0,2,0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        trackingSpaceRot = trackingSpace.eulerAngles;
-        //Test to see if program is in editor
         if(Application.isEditor){
-            emulatedtouchpos = new Vector2(touchX,touchY);
-            if(emulatedtouchpos!=Vector2.zero){
-                rads = Mathf.Atan2(emulatedtouchpos.y,emulatedtouchpos.x)-trackingSpaceRot.y*Mathf.Deg2Rad;
-                degs = rads*Mathf.Rad2Deg;
-                velocity = new Vector3(Mathf.Cos(rads),0,Mathf.Sin(rads));
-                Vector3 translation = (velocity/100)*Vector2.Distance(Vector2.zero,emulatedtouchpos)*speed;
-                transform.Translate(translation);
+            if(Input.GetKey(KeyCode.Escape)){
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None; 
             }
-
-            //sword movement
-           //sword.transform.eulerAngles = new Vector3(controller.eulerAngles.x,head.eulerAngles.y+controller.eulerAngles.y,sword.transform.eulerAngles.z);
-           //handle.transform.eulerAngles = new Vector3(handle.transform.eulerAngles.x,handle.transform.eulerAngles.y,controller.rotation.eulerAngles.z);
-            //sword.transform.rotation = new Quaternion(0,0,emulatedControllerRotZ,0);
-           // sword.transform.rotation = Quaternion.Euler(emulatedControllerRotX,emulatedControllerRotY,0); 
-            //sword2.transform.localRotation = Quaternion.Euler(sword2.transform.localRotation.x,0,emulatedControllerRotZ);
-            //sword.transform.Rotate(emulatedControllerRotX,emulatedControllerRotY,emulatedControllerRotZ);
+            transform.Rotate(0,Input.GetAxis("Horizontal"),0);
+            transform.Translate(0,0,Input.GetAxis("Vertical")/50);
+            controller.transform.Rotate(-Input.GetAxis("Mouse Y"),Input.GetAxis("Mouse X"),0);
+            if(Input.GetMouseButton(0)){
+                if(hand.transform.parent.GetComponent<Animation>().clip == hand.transform.parent.GetComponent<Animation>().GetClip("HandOpen")){
+                    hand.transform.parent.GetComponent<Animation>().Stop();
+                    hand.transform.parent.GetComponent<Animation>().clip = hand.transform.parent.GetComponent<Animation>().GetClip("HandClose");
+                    hand.transform.parent.GetComponent<Animation>().Play(); 
+                }
+                if(hand.GetComponent<CollisionDetector>().colliding&&this.equippedWeapon==null){
+                    if(hand.GetComponent<CollisionDetector>().GetCollidedObject().transform.gameObject!=equippedWeapon&&hand.GetComponent<CollisionDetector>().GetCollidedObject().transform.tag=="Weapon"){
+                        hand.GetComponent<CollisionDetector>().GetCollidedObject().GetComponent<Rigidbody>().isKinematic = true;
+                        this.equippedWeapon = hand.GetComponent<CollisionDetector>().GetCollidedObject().transform.gameObject;
+                        
+                        this.equippedWeapon.transform.parent = gripPoint.transform;
+                        this.equippedWeapon.transform.localPosition = Vector3.zero;
+                        this.equippedWeapon.transform.localRotation = Quaternion.Euler(0,0,90);
+                    }
+                }
+            }else{
+                if(hand.transform.parent.GetComponent<Animation>().clip == hand.transform.parent.GetComponent<Animation>().GetClip("HandClose")){
+                    hand.transform.parent.GetComponent<Animation>().Stop();
+                    hand.transform.parent.GetComponent<Animation>().clip = hand.transform.parent.GetComponent<Animation>().GetClip("HandOpen");
+                    hand.transform.parent.GetComponent<Animation>().Play();
+                    if(this.equippedWeapon!=null){
+                        this.equippedWeapon.transform.GetComponent<Rigidbody>().isKinematic = false;
+                        this.equippedWeapon.transform.parent = null;
+                        this.equippedWeapon = null;
+                    }
+                }
+            }
         }else{
+            
+            trackingSpaceRot = trackingSpace.eulerAngles;
+
+                //sword movement
+            //sword.transform.eulerAngles = new Vector3(controller.eulerAngles.x,head.eulerAngles.y+controller.eulerAngles.y,sword.transform.eulerAngles.z);
+            //handle.transform.eulerAngles = new Vector3(handle.transform.eulerAngles.x,handle.transform.eulerAngles.y,controller.rotation.eulerAngles.z);
+                //sword.transform.rotation = new Quaternion(0,0,emulatedControllerRotZ,0);
+            // sword.transform.rotation = Quaternion.Euler(emulatedControllerRotX,emulatedControllerRotY,0); 
+                //sword2.transform.localRotation = Quaternion.Euler(sword2.transform.localRotation.x,0,emulatedControllerRotZ);
+                //sword.transform.Rotate(emulatedControllerRotX,emulatedControllerRotY,emulatedControllerRotZ);
+            if(OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger)){
+                if(hand.transform.parent.GetComponent<Animation>().clip == hand.transform.parent.GetComponent<Animation>().GetClip("HandOpen")){
+                    hand.transform.parent.GetComponent<Animation>().Stop();
+                    hand.transform.parent.GetComponent<Animation>().clip = hand.transform.parent.GetComponent<Animation>().GetClip("HandClose");
+                    hand.transform.parent.GetComponent<Animation>().Play(); 
+                }
+                if(hand.GetComponent<CollisionDetector>().colliding&&this.equippedWeapon==null){
+                    if(hand.GetComponent<CollisionDetector>().GetCollidedObject().transform.gameObject!=equippedWeapon&&hand.GetComponent<CollisionDetector>().GetCollidedObject().transform.tag=="Weapon"){
+                        hand.GetComponent<CollisionDetector>().GetCollidedObject().GetComponent<Rigidbody>().isKinematic = true;
+                        this.equippedWeapon = hand.GetComponent<CollisionDetector>().GetCollidedObject().transform.gameObject;
+                        
+                        this.equippedWeapon.transform.parent = gripPoint.transform;
+                        this.equippedWeapon.transform.localPosition = Vector3.zero;
+                        this.equippedWeapon.transform.localRotation = Quaternion.Euler(0,0,90); 
+                    }
+                }
+            }else{
+                if(hand.transform.parent.GetComponent<Animation>().clip == hand.transform.parent.GetComponent<Animation>().GetClip("HandClose")){
+                    hand.transform.parent.GetComponent<Animation>().Stop();
+                    hand.transform.parent.GetComponent<Animation>().clip = hand.transform.parent.GetComponent<Animation>().GetClip("HandOpen");
+                    hand.transform.parent.GetComponent<Animation>().Play();
+                    if(this.equippedWeapon!=null){
+                        this.equippedWeapon.transform.GetComponent<Rigidbody>().isKinematic = false;
+                        this.equippedWeapon.transform.parent = null; 
+                        this.equippedWeapon = null;
+                    }
+                }
+            }
             //gets the finger position on the gearvr controller touchpad
             Vector2 touches = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad, OVRInput.Controller.RTrackedRemote);
             //if not touching the pad we dont want to move
@@ -70,13 +131,14 @@ public class Movement : MonoBehaviour
                 velocity = new Vector3(Mathf.Cos(rads),0,Mathf.Sin(rads));
                 transform.Translate((velocity/100)*Vector2.Distance(Vector2.zero,touches)*speed*5);
             }
-            
-            //sword movement
-           //sword.transform.eulerAngles = new Vector3(controller.eulerAngles.x,head.eulerAngles.y+controller.eulerAngles.y,sword.transform.eulerAngles.z);
-           //handle.transform.eulerAngles = new Vector3(handle.transform.eulerAngles.x,handle.transform.eulerAngles.y,controller.rotation.eulerAngles.z);
-            //sword.transform.rotation = new Quaternion(sword.transform.rotation.x,sword.transform.rotation.y,OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote).z,0);
-            //sword2.transform.localRotation = new Quaternion(sword2.transform.localRotation.x,0,OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote).z,0);
-            //OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+                
+                //sword movement
+            //sword.transform.eulerAngles = new Vector3(controller.eulerAngles.x,head.eulerAngles.y+controller.eulerAngles.y,sword.transform.eulerAngles.z);
+            //handle.transform.eulerAngles = new Vector3(handle.transform.eulerAngles.x,handle.transform.eulerAngles.y,controller.rotation.eulerAngles.z);
+                //sword.transform.rotation = new Quaternion(sword.transform.rotation.x,sword.transform.rotation.y,OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote).z,0);
+                //sword2.transform.localRotation = new Quaternion(sword2.transform.localRotation.x,0,OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote).z,0);
+                //OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+       
         }
     }
 }
